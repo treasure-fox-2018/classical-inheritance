182d365eb6b8dd199027b1c0ff7a45d18e431316
Yusuf Salman Noerinaldy Siregar <yusufsiregar5@gmail.com>
2018-06-04 21:44:29 +0700

    finish release 0 & release 1

--- index.js
+++ index.js
@@ -1,1 +1,76 @@
 "use strict"
+
+class Animal {
+  constructor() {
+    this._alive = true;
+    this._creationOfGod = true;
+    this.SuperPower = new SuperPower;
+  }
+
+  roar() {
+    return "AAAAAAA";
+  }
+
+}
+
+class Herbivorae extends Animal {
+  constructor(numberOfLegs) {
+    super(numberOfLegs);
+    this._order = "Herbivorae"
+    this.legs = numberOfLegs;
+  }
+}
+
+class Carnivorae extends Animal {
+  constructor(numberOfLegs) {
+    super(numberOfLegs);
+    this._order = "Carnivorae"
+    this.legs = numberOfLegs;
+  }
+}
+
+class Panthera extends Carnivorae {
+  constructor(numberOfLegs) {
+    super(numberOfLegs);
+    this.legs = 4;
+  }
+
+  roar () {
+    return "GROARRRR";
+  }
+
+}
+
+class SuperPower {
+  constructor() {
+    this._defaultPower = "ORDINARY ANIMAL"
+    return;
+  }
+
+  get laser() {
+    this._laserVision = "CUINGGGGGGGG";
+    return this._laserVision;
+  }
+
+  get meepo() {
+    this._meepo = "DIVIDED WE STAND";
+    return this._meepo;
+  }
+
+  get silencer() {
+    this._silenced = "SILENCE";
+    return this._silenced;
+  }
+}
+
+var zebra = new Herbivorae();
+
+console.log(zebra.roar());
+
+var lion = new Panthera();
+
+var power = new SuperPower();
+
+console.log(lion.SuperPower);
+
+// console.log(lion.roar());
