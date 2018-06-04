be1ee031bff061fb1bbd849b287904453e13a8ed
rizariza69 <riza.riza69@gmail.com>
2018-06-04 21:54:55 +0700

    inherite

--- index.js
+++ index.js
@@ -1,1 +1,73 @@
 "use strict"
+
+class Animal {
+  constructor(leg, lifeType){
+    this.leg = leg
+    this.lifeType = lifeType
+    this.superPower = new superPower()
+  }
+}
+
+class Dolpin extends Animal {
+  constructor(leg,lifeType){
+    super(leg,lifeType)
+  }
+}
+
+class Bat extends Animal {
+  constructor(leg,lifeType){
+    super(leg,lifeType)
+  }
+}
+
+class Fox extends Animal {
+  constructor(leg,lifeType){
+    super(leg,lifeType)
+  }
+}
+
+class Chimpanze extends Animal {
+    constructor(leg,lifeType) {
+        super(leg,lifeType)
+
+    }
+}
+
+class Chicken extends Animal {
+    constructor(leg,lifeType) {
+      super(leg,lifeType)
+
+    }
+}
+
+class superPower {
+    constructor() {
+        this.superPower = 'this animal is doesnt have any superpower'
+    }
+    sonar() {
+        this.superPower = 'have ultra sonar';
+    }
+    fastRunning() {
+        this.superPower = 'have fast running'
+    }
+    seeInTheDark() {
+        this.superPower = 'see in the dark';
+    }
+}
+
+let dolpin = new Dolpin(2, 'water')
+let bat = new Bat(2, 'cave')
+let fox = new Fox(4, 'land')
+let chimpanze = new Chimpanze(4, 'land')
+let chicken = new Chicken(2,'land')
+
+dolpin.superPower.sonar()
+chimpanze.superPower.fastRunning()
+bat.superPower.seeInTheDark()
+fox.superPower.seeInTheDark()
+
+console.log(dolpin)
+console.log(bat)
+console.log(fox)
+console.log(chimpanze)
+console.log(chicken)
