627b703be79be72993368b7afe09e13be30b4f0d
handipriyono <handimacbook@gmail.com>
2018-06-04 20:52:03 +0700

    add inheritance

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 "use strict"
+class Animal {
+  constructor(legs, name) {
+    this.legs = legs
+    this.name = name
+    this.superpower = new SuperPower()
+  }
+}
+
+class Ayam extends Animal {
+  constructor(legs, name) {
+    super(legs, name)
+  }
+}
+
+class Frog extends Animal {
+  constructor(legs, name) {
+    super(legs, name)
+  }
+}
+
+class Batman extends Animal {
+  constructor(legs, name) {
+    super(legs, name)
+  }
+}
+
+class SuperPower {
+  use_laser_vision() {
+    return ' Using laser vison! '
+
+  }
+
+  be_invisible() {
+    return 'become Invisible ! '
+  }
+
+}
+
+
+
+// let animalku = new Animal(5, 'coba')
+// console.log(animalku)
+let ayaman = new Ayam(4, 'susi')
+let batm = new Batman(4, 'Kingblack')
+console.log(batm)
+console.log(ayaman)
+console.log(ayaman.name, ayaman.superpower.use_laser_vision())
