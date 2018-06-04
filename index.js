e505456beda20fbdcbdacf6a6c2819e4fc4defed
HelmiYogantara <helmiyogantara@gmail.com>
2018-06-04 22:24:59 +0700

    exercise done

--- index.js
+++ index.js
@@ -1,1 +1,95 @@
 "use strict"
+class Animal {
+    constructor(mata, kaki){
+        this.mata = mata
+        this.kaki = kaki
+        this.superpower = new SuperPower
+    }
+}
+
+class Frog extends Animal{
+    constructor(mata, kaki, amphibi){
+        super(mata, kaki)
+        this.amphibi = amphibi
+    }
+
+    suara(){
+        console.log('ngoooook');
+    }
+}
+
+class Bat extends Animal{
+    constructor(mata,kaki,aves){
+        super(mata,kaki)
+        this.aves = aves
+    }
+
+    suara(){
+        console.log('ngiiiiiiikkk');
+        
+    }
+}
+
+class Chimpanze extends Animal{
+    constructor(mata, kaki,mamalia){
+        super(mata, kaki, mamalia)
+        this.mamalia = mamalia
+    }
+
+    suara(){
+        console.log('uuu aaaa uuuu aaaa');
+        
+    }
+}
+
+class Fox extends Animal{
+    constructor(mata, kaki, karnivora){
+        super(mata, kaki)
+        this.karnivora = karnivora
+    }
+
+    suara(){
+        console.log('auuuuuuuuuuuuuuu');
+        
+    }
+}
+
+class Chicken extends Animal{
+    constructor(mata, kaki, insectiphora){
+        super(mata, kaki)
+        this.insectiphora = insectiphora
+    }
+    
+    suara(){
+        console.log('ciak ciak ciak');
+        
+    }
+}
+
+class SuperPower {
+    constructor(){
+        this.superpower = 'be the hero'
+    }
+
+    bite(){
+        this.superpower = 'this animal has a serious biting'
+    }
+
+    punch(){
+        this.superpower = 'this animal has a strong punch'
+    }
+}
+
+
+var ayam = new Chicken('ada','ada','iya')
+ayam.suara()
+console.log(ayam.mata);
+var kodok = new Frog('ada','ada','iya')
+kodok.suara()
+var serigala = new Fox('ada','ada','iya')
+serigala.suara()
+
+serigala.superpower.bite()
+console.log(serigala);
+
+
