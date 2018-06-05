eb2907df17beeb7635a86d2909ba62f9f6b139f1
ronifajrian <ronifajrian@gmail.com>
2018-06-06 05:13:52 +0700

    add inheritance

--- index.js
+++ index.js
@@ -1,1 +1,72 @@
 "use strict"
+
+class Animal {
+  constructor (habitat, tipe) {
+    // this._classAnimal = classAnimal;
+    this._habitat = habitat
+    // this._tipe = 'mamalia' // test assign
+    this._tipe = tipe
+    this._superPower = new SuperPower();
+  }
+
+  get habitat(){
+    return this._habitat
+  }
+
+  get tipe(){
+    return this._tipe
+  }
+
+  get superPower(){
+    return this._superPower
+  }
+}
+
+class Frog extends Animal{
+  constructor(){
+    super('darat','amfibi')
+    this._isHaveLeg = true
+  }
+
+  get isHaveLeg(){
+    return this._isHaveLeg
+  }
+
+}
+
+class Bat extends Animal{
+  constructor(){
+    super('goa','mamalia')
+    this._isHaveWings = true
+  }
+
+  get isHaveWings(){
+    return this._isHaveWings
+  }
+}
+
+class SuperPower {
+  constructor () {
+    
+  }
+
+  powerFrog () {
+    console.log('---- ini super Power Frog');
+    
+  }
+
+  powerBat () {
+    console.log('---- ini Super Power Bat');
+    
+  }
+}
+
+
+var frog = new Frog()
+var bat = new Bat()
+
+console.log(frog);
+console.log(bat);
+
+frog.superPower.powerFrog()
+bat.superPower.powerBat()
